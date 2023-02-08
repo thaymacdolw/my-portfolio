e38773a127d476bdb4bccfffee408f829deb0b4a
Thayna Mac Dolw <thaymacdolw@gmail.com>
2023-02-08 19:25:32 -0300

    animations and section 1 finished

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// 
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.title1', { duration: 1500 });
+sr.reveal('.title2', { duration: 1500 });
+sr.reveal('.title3', { duration: 1500 });
+sr.reveal('.title4', { duration: 1500 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.portfolio');
+hiddenElements.forEach((el) => observer.observe(el));
