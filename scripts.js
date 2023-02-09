a7876777ff8f02ad3247dc4f1964cb856f2bd404
Thayna Mac Dolw <thaymacdolw@gmail.com>
2023-02-09 01:13:30 -0300

    nav bar issue solved

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// 
+//tittle animations
 window.sr = ScrollReveal({reset: true});
 
 sr.reveal('.title1', { duration: 1500 });
@@ -6,6 +6,7 @@ sr.reveal('.title2', { duration: 1500 });
 sr.reveal('.title3', { duration: 1500 });
 sr.reveal('.title4', { duration: 1500 });
 
+// Projects Animations
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
