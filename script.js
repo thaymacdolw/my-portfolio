cde3a1d4a1439604ac9b78b0e5c8f79c0fee9cb1
Thayna Mac Dolw <thaymacdolw@gmail.com>
2024-05-28 23:28:08 +0200

    Sticky navbar finished

--- script.js
+++ script.js
@@ -13,3 +13,9 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+// Sticky Navbar
+window.onscroll = () =>  {
+let header = document.querySelector('.header');
+header.classList.toggle('sticky', window.scrollY > 100);
+}
