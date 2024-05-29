f3278e37e25f52c2aa6878baecb1ba6065940ff4
Thayna Mac Dolw <thaymacdolw@gmail.com>
2024-05-30 00:59:28 +0200

    dark mode finished

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove("active");
-                document.querySelector("header nav a[href*=" + id +"]").classList.add("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
             });
         };
     });
 
-//---------Sticky Navbar------------
-    
+    //---------Sticky Navbar------------
+
     let header = document.querySelector('.header');
-    
+
     header.classList.toggle('sticky', window.scrollY > 100);
 };
-    
+
 //-------OnClick feature on About Section-----------
 
 var tablinks = document.getElementsByClassName("tab-links");
@@ -69,17 +69,17 @@ const validateForm = (form) => {
         valid = false;
     }
 
-   //E-mail Validation
-   let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-   let emailValue = email.value;
-   if (!emailRegex.test(emailValue)){
-    giveError(email, "Please enter a valid email");
-    valid = false;
-   }
-   //to return true if email is valid
-   if(valid) {
-    return true; //!!!!!personalizar janela pop-out!!!!!!
-   }
+    //E-mail Validation
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let emailValue = email.value;
+    if (!emailRegex.test(emailValue)) {
+        giveError(email, "Please enter a valid email");
+        valid = false;
+    }
+    //to return true if email is valid
+    if (valid) {
+        return true; //!!!!!personalizar janela pop-out!!!!!!
+    }
 };
 
 const giveError = (field, message) => {
@@ -87,7 +87,7 @@ const giveError = (field, message) => {
     parentElement.classList.add("error");
     //if the error message already exists, it'll not repeat it
     let existingError = parentElement.querySelector(".err-msg")
-    if(existingError) {
+    if (existingError) {
         existingError.remove();
     }
     let error = document.createElement("span")
@@ -99,7 +99,7 @@ const giveError = (field, message) => {
 const inputs = document.querySelectorAll("input");
 const textareas = document.querySelectorAll("textarea");
 
-let allFields = [... inputs, ...textareas];
+let allFields = [...inputs, ...textareas];
 
 allFields.forEach((field) => {
     field.addEventListener("input", () => {
@@ -110,7 +110,71 @@ const removeError = (field) => {
     let parentElement = field.parentElement;
     parentElement.classList.remove("error");
     let error = parentElement.querySelector(".err-msg");
-    if(error) {
+    if (error) {
         error.remove();
     }
 };
+
+//----sticky scroll "back to top" button------
+
+const scrollToTopButton = document.getElementById('js-top');
+let scrollTimeout;
+//creating a function that shows the scroll-to-top button if the user scrolls beyond the height of the initial window.
+const scrollFunc = () => {
+    // Get the current scroll value
+    let y = window.scrollY;
+
+    // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+    if (y > 0) {
+        scrollToTopButton.classList.add("show");
+        scrollToTopButton.classList.remove("hide");
+    } else {
+        scrollToTopButton.classList.add("hide");
+        scrollToTopButton.classList.remove("show");
+    }
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+        scrollToTopButton.classList.add("hide");
+        scrollToTopButton.classList.remove("show");
+    }, 3000); // 3 segundos
+};
+
+window.addEventListener("scroll", scrollFunc);
+const scrollToTop = () => {
+    // Let's set a variable for the number of pixels we are from the top of the document.
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+
+    // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+    // We'll also animate that scroll with requestAnimationFrame:
+    // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        // ScrollTo takes an x and a y coordinate.
+        // Increase the '10' value to get a smoother/slower scroll!
+        window.scrollTo(0, c - c / 10);
+    };
+};
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function (e) {
+    e.preventDefault();
+    scrollToTop();
+};
+
+//---------------Dark Mode -------------------
+let darkModeIcon = document.querySelector("#darkMode-icon");
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle("bx-sun");
+    document.body.classList.toggle("dark-mode");
+};
+
+//----------------Scroll Reveal --------------
+ScrollReveal({
+    //reset: true,
+    distance: '80px',
+    duration: 1000,
+    delay: 100
+});
+ScrollReveal().reveal('.home-content, .heading, .subtitle-about', { origin: 'top' });
+ScrollReveal().reveal('.home-img img,.ul-stacks,.portfolio-box,.contact-container', { origin: 'bottom' });
+ScrollReveal().reveal('.about-col-1', { origin: 'left' });
